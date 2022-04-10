d0f4fefb61146c3c20035a8ab107e9f8c0445b42
ANNA-khy <kang01019@naver.com>
2022-04-10 22:19:27 +0900

    import exception

--- Server/src/main/java/com/CommitAlarmSlackBot/CommitAlarmSlackBotApplication.java
+++ Server/src/main/java/com/CommitAlarmSlackBot/CommitAlarmSlackBotApplication.java
@@ -1,12 +1,15 @@
 package com.CommitAlarmSlackBot;
 
+import com.slack.api.methods.SlackApiException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class CommitAlarmSlackBotApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SlackApiException, IOException {
 		SpringApplication.run(CommitAlarmSlackBotApplication.class, args);
 	}
 
